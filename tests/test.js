47145b40063a2abeb0182116a98dfc43eae5045a
mikhail.shevchuk <mikhail.shevchuk@gmail.com>
2013-01-22 17:05:48 +0400

    initial revision, base files were added

---
+++ tests/test.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: mikhail.shevchuk
+ * Date: 22.01.13
+ * Time: 17:04
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ wizlite/wizlite.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: mikhail.shevchuk
+ * Date: 22.01.13
+ * Time: 17:03
+ * To change this template use File | Settings | File Templates.
+ */
