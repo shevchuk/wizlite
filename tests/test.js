5be45cc375865882eb15e2fe7c4f77cc99974502
mikhail.shevchuk <mikhail.shevchuk@gmail.com>
2013-01-22 19:37:04 +0400

    adding basic functionality

--- tests/test.js
+++ tests/test.js
@@ -5,3 +5,62 @@
  * Time: 17:04
  * To change this template use File | Settings | File Templates.
  */
+module("wizlite");
+
+test("Simple add user wiz test (2 sequential steps)", function () {
+    var addUserStep = new WizStep({
+        name: 'addUserStep',
+        onEnter: function(data) {
+
+        },
+        onDone: function() {
+            return {firstName: 'John'};
+        },
+        onBack: function() {
+
+        },
+        onCancel: function() {
+
+        }
+    });
+
+    equal(addUserStep.name, 'addUserStep', 'Check that name for add user step was correctly assigned')
+
+    var johnStep = new WizStep({
+        onEnter: function(data) {
+
+        }
+    });
+
+    // helpers
+    function isJohnEntered() {
+        return w.storage.firstName == 'John';
+
+    };
+
+
+    var w = new Wiz({
+        name: 'wiz',
+        steps: {
+            addUser: {
+                step: addUserStep,
+                onNext: function() {
+                    if (isJohnEntered()) {w.goTo('johnWelcome')};
+                }
+            },
+            johnWelcome: {
+                step: johnStep,
+                onNext: function() {
+
+                }
+            }
+        }
+    });
+
+    w.start();
+
+    equal(w.getCurrentStep().name, 'addUserStep', 'Check that first step is the first');
+    w.next();
+
+
+});
--- wizlite/wizlite.js
+++ wizlite/wizlite.js
@@ -5,3 +5,49 @@
  * Time: 17:03
  * To change this template use File | Settings | File Templates.
  */
+
+function Wiz(config)
+{
+    var self = this;
+    this.config = config || {};
+
+    this.steps = this.config.steps;
+
+    buildSteps();
+
+    function buildSteps() {
+        var steps = Object.keys(self.steps);
+        for (var i = 0; i < steps.length; i++) {
+        }
+    }
+}
+
+Wiz.prototype = {
+    toString: function () {
+        return 'UnnamedWizard';
+    },
+    start: function() {
+
+    },
+    next: function() {
+
+    },
+    getCurrentStep : function() {
+        return 'step1';
+    }
+}
+
+
+function WizStep(config)
+{
+    this.config = config || {};
+
+    this.name = this.config.name || 'Unnamed';
+
+}
+
+WizStep.prototype = {
+    toString:function() {
+        return 'UnnamedWizardStep';
+    }
+}
